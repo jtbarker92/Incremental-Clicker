f51cc8e8d31fd3ca80a4f0fb5ba379577b5b9d4b
jtbarker92 <78832084+jtbarker92@users.noreply.github.com>
2021-03-16 20:05:22 +1100

    Added multiple buttons/functions

--- Assets/Scenes/Scripts/PointManager.cs
+++ Assets/Scenes/Scripts/PointManager.cs
@@ -9,17 +9,33 @@ public class PointManager : MonoBehaviour
     [SerializeField] private int pointsPerClick = 1;
     [SerializeField] private Text pointsText;
     [SerializeField] private Text upgradeText;
+    [SerializeField] private float pointsPerSecond;
+    [SerializeField] private float scoreConverter;
 
-    //updates
+
     private int costToUpdate = 5;
-    public void Update()
-    {u
-        UpdateUpgradeText();
+
+    public void Start()
+    {
+        pointsPerSecond = 10;
     }
+    public void Update()
+    {
+        UpdateUpgradeText();
+
+
+    }
+
+    public void PointsPerSecondUpgrade()
+    {
+        scoreConverter += pointsPerSecond * Time.deltaTime;
+        pointsPerSecond += Mathf.RoundToInt(scoreConverter);
+
+    } 
 
     public void AddPoints()
     {
-        points+= pointsPerClick;
+        points += pointsPerClick;
         pointsText.text = points.ToString();
     }
 
@@ -33,23 +49,62 @@ public class PointManager : MonoBehaviour
             UpdatePointsText();
             UpdateUpgradeText();
         }
-        
+
     }
+
     private void UpdateUpgradeText()
     {
-        if(upgradeText != null)
+        if (upgradeText != null)
         {
-            upgradeText.text = "Upgrade " + costToUpdate.ToString();
+            upgradeText.text = " Upgrade Cost = " + costToUpdate.ToString();
         }
     }
 
+    public void UpgradedPointsPerClick2()
+    {
+        if (points >= costToUpdate && costToUpdate >= 160)
+        {
+            points -= costToUpdate;
+            pointsPerClick = pointsPerClick + 10;
+            costToUpdate = costToUpdate * 2;
+            UpdatePointsText();
+            UpdateUpgradeText();
+        }
+
+    }
+
+    public void UpgradedPointsPerClick3()
+    {
+        if (points >= costToUpdate && costToUpdate >= 2560)
+        {
+            points -= costToUpdate;
+            pointsPerClick = pointsPerClick * 2;
+            costToUpdate = costToUpdate * 3;
+            UpdatePointsText();
+            UpdateUpgradeText();
+        }
+
+    }
+
+    public void UpgradedPointsPerClick4()
+    {
+        if (points >= costToUpdate && costToUpdate >= 10)
+        {
+            points -= costToUpdate;
+            pointsPerSecond = 30;
+            costToUpdate = costToUpdate * 3;
+            UpdatePointsText();
+            UpdateUpgradeText();
+        }
+
+    }
+
+
+    // Updates the points score once called by the Upgrade functions
     private void UpdatePointsText()
     {
         pointsText.text = points.ToString();
-            
+
     }
 
-   
-
-    
 }
